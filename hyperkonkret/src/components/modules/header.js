1dfbae34f33aa39829a68651d6200f6ad69eb206
matildatrodin <matilda@trodin.com>
2018-04-12 17:11:37 +0200

    la till en slideshow

--- hyperkonkret/src/components/modules/header.js
+++ hyperkonkret/src/components/modules/header.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from '../../images/logo.svg';
 
 export class Header extends React.Component{
 
     render(){
         return(
-            <header className="App-header">
-                <h1><b>Hyperkonkret!</b></h1>
-            </header>
+            <header className="App-header"/>
         );
     }
 
--- hyperkonkret/src/components/pages/firstPage.js
+++ hyperkonkret/src/components/pages/firstPage.js
@@ -1,41 +1,97 @@
 import React from 'react';
+import '../../images/img2.jpg';
+import '../../images/img2.jpg';
 import '../../styles/firstPage.css'
-import { Alert } from 'reactstrap';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+} from 'reactstrap';
 
-export class FirstPage extends React.Component{
+const items = [
+    {
+        image: require('../../images/img3.jpg'),
+        /*altText: 'Slide 1',
+        caption: 'Slide 1'*/
+    },
+    {
+        image: require('../../images/img2.jpg'),
+        /*altText: 'Slide 2',
+        caption: 'Slide 2'*/
+    },
+    {
+        image: require('../../images/img1.jpg'),
+        /*altText: 'Slide 3',
+        caption: 'Slide 3'*/
+    }
+];
+
+export class FirstPage extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            visible: true
-        };
-
-        this.onDismiss = this.onDismiss.bind(this);
+        this.state = { activeIndex: 0 };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
     }
 
-    onDismiss() {
-        this.setState({ visible: false });
+    onExiting() {
+        this.animating = true;
     }
 
-    render(){
-        return(
-            <div>
-                <Alert color="dark">
-                    <h4 className="alert-heading" align="left">Kul med saker</h4>
-                    <p align="left">
-                        Här kan man skriva massa saker som säger massa saker om andra saker och det är kul.
-                    </p>
-                    <hr />
-                    <p className="mb-0" align="left">
-                        Här är ett stycke till som är avgränsat av ett fint litet sträck, kul va?
-                    </p>
-                </Alert>
-                <Alert color="info" isOpen={this.state.visible} toggle={this.onDismiss}>
-                    Jag är ett meddelande som kan klickas bort!
-                </Alert>
-            </div>
+    onExited() {
+        this.animating = false;
+    }
+
+    next() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    previous() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    goToIndex(newIndex) {
+        if (this.animating) return;
+        this.setState({ activeIndex: newIndex });
+    }
+
+    render() {
+        const { activeIndex } = this.state;
+
+        const slides = items.map((item) => {
+            return (
+                <CarouselItem
+                    onExiting={this.onExiting}
+                    onExited={this.onExited}
+                    key={item.image}
+                >
+                    <img src={item.image} alt={item.altText} />
+                    <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+                </CarouselItem>
+            );
+        });
+
+        return (
+            <Carousel
+                activeIndex={activeIndex}
+                next={this.next}
+                previous={this.previous}
+            >
+                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+            </Carousel>
         );
     }
-
 }
