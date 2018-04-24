82fd590b1deaf94514bd625da7713ed06618c0da
slumpmassigt <victoriabull@live.se>
2018-04-24 15:19:06 +0200

    hej

---
+++ hyperkonkret/src/components/pages/course2.js
@@ -0,0 +1,48 @@
+import QuizApp from '../../quiz/src/components/QuizApp';
+import React from 'react';
+import '../../quiz/src/style.css';
+import { GetQuiz } from '../../quiz/src/components/getQuiz';
+
+export class Course extends React.Component {
+    constructor() {
+        super();//ta in props för vilken kurs/quiz som ska visas?
+
+        this.state = {
+            show: 1
+        }
+    }
+
+
+    handleClick(showQuiz){ //på vilken quiz du vill göra
+        if (showQuiz == 1){
+            this.setState({show: 2 });
+
+        }
+        else if (showQuiz == 2){
+            this.setState({ show: 1 });
+
+        }
+
+
+    }
+
+
+    render() {
+        if(this.state.show == 1){
+            return(
+                <div>
+                    <GetQuiz id={1}/>
+                </div>);
+        }
+        else if (this.state.show == 2){
+            return(
+                <div>
+                    <GetQuiz id={2} />
+                </div>
+            )
+        }
+        else{
+            return(<h1>Finns inget sådant quiz!</h1>);
+        }
+    }
+}
