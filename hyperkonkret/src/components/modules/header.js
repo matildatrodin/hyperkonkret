f7dc1129f235f8c13ed949683d4a457ce9200233
vanjaw <vanja.wallner@me.com>
2018-05-10 15:13:42 +0200

    La in logotyperna

--- hyperkonkret/src/components/modules/header.js
+++ hyperkonkret/src/components/modules/header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+//import Heck from '../../images/logo shadow/hyperkonkret-black.png';
 export class Header extends React.Component{
 
     render(){
         return(
             <header className="App-header"/>
+          //  <img src={Heck}/>
         );
     }
 
