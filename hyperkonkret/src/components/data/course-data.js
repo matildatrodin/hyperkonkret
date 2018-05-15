50e19cf7a3cff871057824dd7a117e1f287f9c11
carolinealgvere <caroline.algvere@hotmail.com>
2018-05-15 09:00:41 +0200

    fixat dina-ämnen-sidan

--- hyperkonkret/src/components/data/course-data.js
+++ hyperkonkret/src/components/data/course-data.js
@@ -2,7 +2,7 @@ export const course =
     [{
         id: 1,
         title: 'Landskap',
-        description: 'Landskap är geografiska områden som vanligen bygger på en äldre politisk indelning och är grundad på kulturella och geografiska särdrag som många människor starkt identifierar sig med än i dag.',
+        description: 'Landskap är geografiska områden som vanligen bygger på en äldre politisk indelning och är grundad på kulturella och geografiska särdrag som många människor starkt identifierar sig med än i dag. Här lär du dig om en del av Sveriges uppdelning av mark, de så kallade landskapen.',
         subject: 'Geografi',
         created_by: 'Björn',
         created_at: '20180103',
@@ -11,7 +11,7 @@ export const course =
     {
         id: 3,
         title: 'Ekonomiska system',
-        description: 'Ekonomi-info',
+        description: 'Ekonomi är läran om att hantera pengar då man inte har hur mycket som helst. Detta ämne tar dig igenom allt du behöver veta för att klara av att hantera pengar.',
         subject: 'Ekonomi',
         created_by: 'Björn',
         created_at: '20180411',
@@ -20,7 +20,7 @@ export const course =
     {
         id: 3,
         title: 'Medeltiden',
-        description: 'Med medeltiden avses perioden från antikens slut till renässansens början, ungefär från 500 e.Kr till 1500 e.Kr (men Nordens medeltid räknas från ca 1050 till 1520).',
+        description: 'Med medeltiden avses perioden från antikens slut till renässansens början, ungefär från 500 e.Kr till 1500 e.Kr (men Nordens medeltid räknas från ca 1050 till 1520). Här lär du dig om dena historiska tid',
         subject: 'Historia',
         created_by: 'Björn',
         created_at: '20180213',
--- hyperkonkret/src/components/pages/courseList.js
+++ hyperkonkret/src/components/pages/courseList.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 import Course from './course';
 import {Header} from '../modules/header';
 import {course} from '../data/course-data';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Col, CardColumns, Row, Container, CardDeck, CardLink } from 'reactstrap';
+import '../../styles/myCourses.css';
 
+const img = {
+    landskap: require('../../images/icons/tummeupp.png'),
+};
 
 export default class CourseList extends React.Component{
     constructor(props){
@@ -29,13 +34,49 @@ export default class CourseList extends React.Component{
             return (
                 <div>
                     <Header/>
-                    <h2>Dina registrerade kurser:</h2>
-                    <br></br>
-                    <Button style={{marginBottom: '2%'}} onClick={(id) => this.handleClick(1)}>{course[0].title}</Button>
-                    <br/>
-                    <Button style={{marginBottom: '2%'}} onClick={(id) => this.handleClick(2)}>{course[1].title}</Button>
-                    <br/>
-                    <Button style={{marginBottom: '2%'}} onClick={(id) => this.handleClick(3)}>{course[2].title}</Button>
+                    <div className="title-subjects">
+                        <h2>Dina ämnen</h2>
+                    </div>
+                    <div className="subjects">
+                        <CardDeck>
+                            <Card className="card-box">
+                                <CardImg style={{marginLeft: '25%', marginRight: '25%', marginTop: '6%', width: '50%'}} src={img.landskap}/>
+                                <CardBody>
+                                    <CardTitle>{course[0].title}</CardTitle>
+                                    <CardText>
+                                        {course[0].description}
+                                    </CardText>
+                                    <CardLink >
+                                        <Button style={{marginBottom: '2%'}} onClick={(id) => this.handleClick(1)}>Gör en övning</Button>
+                                    </CardLink>
+                                </CardBody>
+                            </Card>
+                            <Card>
+                                <CardImg/>
+                                <CardBody>
+                                    <CardTitle>{course[1].title}</CardTitle>
+                                    <CardText>
+                                        {course[1].description}
+                                    </CardText>
+                                    <CardLink >
+                                        <Button style={{marginBottom: '2%'}} onClick={(id) => this.handleClick(2)}>Gör en övning</Button>
+                                    </CardLink>
+                                </CardBody>
+                            </Card>
+                            <Card>
+                                <CardImg/>
+                                <CardBody>
+                                    <CardTitle>{course[2].title}</CardTitle>
+                                    <CardText>
+                                        {course[2].description}
+                                    </CardText>
+                                    <CardLink >
+                                        <Button style={{marginBottom: '2%'}} onClick={(id) => this.handleClick(3)}>Gör en övning</Button>
+                                    </CardLink>
+                                </CardBody>
+                            </Card>
+                        </CardDeck>
+                    </div>
                 </div>
 
             )
