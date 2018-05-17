5812d63673d1739a80d8b564d004a0b4c9babb83
slumpmassigt <victoriabull@live.se>
2018-05-17 14:40:05 +0200

    footer fix

--- hyperkonkret/src/components/modules/footer.js
+++ hyperkonkret/src/components/modules/footer.js
@@ -6,16 +6,12 @@ export class Footer extends React.Component{
         return(
             <footer className="App-footer">
             <div className="App-footer-text">
-            Vi tycker det är en självklarhet att alla ska kunna lära sig vad som helst.
-            Vi vill skapa en lärandemiljö som hjälper lärarna att hjälpa elever.
-            Inspiration, motivation och studieteknik i ett paket.
-
-            Our mission is to provide a world-class education to anyone,
-            anywhere that earlier had problems learning.
+               Inspiration, motivation och studieteknik i ett paket.
             </div>
             <div className="App-footer-buttons">
-            <button>FAQ</button>
-            <button>Support</button>
+                <button className="Footer-button">FAQ</button>
+                <button className="Footer-button">Support</button>
+                <button className="Footer-button">Samarbeten</button>
             </div>
             </footer>
         );
