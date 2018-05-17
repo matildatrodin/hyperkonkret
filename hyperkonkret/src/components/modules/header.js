2377d9a26245494a0223001c5f4633d1d7d7711d
vanjaw <vanja.wallner@me.com>
2018-05-17 14:51:22 +0200

    fixat error

--- hyperkonkret/src/components/modules/header.js
+++ hyperkonkret/src/components/modules/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from '../modules/routes';
 import { Link, Redirect, withRouter } from 'react-router-dom';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const images = {
     image: require('../../images/logo shadow/hyperkonkret-black.png')
@@ -21,7 +21,9 @@ export class Header extends React.Component{
 
         return(
             <header className="App-header">
+            <Router>
                 <Link to="/"><img src={images.image} className="App-header-logo"/></Link>
+            </Router>
             </header>
         );
     }
